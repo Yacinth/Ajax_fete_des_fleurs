b5d38dc8663276333bef9b1d265c7674a6b8b1b4
Yacinth <fevriercjjc@gmail.com>
2019-03-01 04:37:22 +0100

    Wednesday 3

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,4 +3,37 @@ class EmailController < ApplicationController
   def index
     @emails = Email.all
   end
+
+    def show
+    @email = Email.find(params[:id])
+    respond_to do |format|
+      format.html{ redirect_to root_path }
+      format.js
+    end
+  end
+  
+  def edit
+    
+    @email = Email.find(params[:id])
+    puts "O"*70
+    respond_to do |format|
+    format.html{ redirect_to root_path }
+    format.js
+    end
+  end
+
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+      end
+  end
+
+    private
+
+  def email_params
+    params.permit(:object, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "email#index"
   resources :tasks, except: [:show]
+  resources :email, only: [:index, :show, :edit, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
